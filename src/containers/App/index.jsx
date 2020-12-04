7aff3305bdc50fdb3c6029d27703263d0c85795b
Roman <roboquasar@gmail.com>
2020-12-04 23:03:01 +0300

    Added pre-commit hook for linter , added linter, Added React Router (#2)

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,12 +1,35 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import AuthPage from 'pages/AuthPage';
+import MainPage from 'pages/MainPage';
+import ProfilePage from 'pages/ProfilePage';
+
 import styles from './styles.module.scss';
 
 function App() {
   return (
-    <div className={styles.main}>
-      <header className={styles.header}>
-        тут будет проект
-      </header>
-    </div>
+    <main className={styles.main}>
+      <Router>
+        <Switch>
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+
+          <Route path="/main">
+            <MainPage />
+          </Route>
+
+          <Route path="/profile">
+            <ProfilePage />
+          </Route>
+        </Switch>
+      </Router>
+    </main>
   );
 }
 
---
+++ src/pages/AuthPage/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AuthPage = () => {
+  return <h1>тут будет AuthPage</h1>
+};
+
+export default AuthPage;
