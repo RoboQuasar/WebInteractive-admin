c87187ae8ef9bbf67629c477f20e7397336803f2
Sokol <silvana93@mail.ru>
2021-02-11 07:27:32 +0300

    Утреняя зарядка для мозга

--- src/components/AuthWindow/index.jsx
+++ src/components/AuthWindow/index.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-//import styles from './styles.module.scss';
+import styles from './styles.module.scss';
+import NavigationLink from 'components/NavigationLink';
 
 const AuthWindow = () => {
   return (
-    <form>
-      <p>Добро пожаловать в систему оживления сайтов!</p>
-     <label htmlFor="email">Логин</label> <input type="email" /><br />
-     <label htmlFor="password">Пароль</label> <input type="password" />
-     <input type="submit" value="войти"/>
 
+    <form className={styles.test}>
+      <fieldset className={styles.authWindow}>
+        <p>Добро пожаловать<br />в систему оживления сайтов!</p>
+        <label htmlFor="email">Логин</label> <input type="email" /><br />
+        <label htmlFor="password">Пароль</label> <input type="password" />
+        <button>Войти</button>
+        <button>Регистрация</button>
+        <NavigationLink hrefLink="/">Узнать о нас больше</NavigationLink>
+      </fieldset>
     </form>
+
   );
 };
 
