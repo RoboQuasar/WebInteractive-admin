1d26e427cf51c67197fc9c2ed3bcf4cf2b22ba04
Sokol <silvana93@mail.ru>
2021-02-16 23:17:31 +0300

    Финальное тестирование

--- src/components/AuthWindow/index.jsx
+++ src/components/AuthWindow/index.jsx
@@ -6,15 +6,15 @@ import NavigationLink from 'components/NavigationLink';
 
 const AuthWindow = () => {
   return (
-    <section className={styles.pageLayout}>
+    <div className={styles.pageLayout}>
       <form className={styles.authWindow}>
         <h1 className={styles.greetingText}>Добро пожаловать<br />в систему оживления сайтов!</h1>
         <div className={styles.wrapInput}>
-          <label id="emailField" htmlFor="email" className={styles.labelText}>Логин</label>
+          <label htmlFor="emailField" className={styles.labelText}>Логин</label>
           <input id="emailField" type="email" className={styles.inputField}/>
         </div>
         <div className={styles.wrapInput}>
-          <label id="passwordField" htmlFor="password" className={styles.labelText}>Пароль</label>
+          <label htmlFor="passwordField" className={styles.labelText}>Пароль</label>
           <input id="passwordField" type="password" className={styles.inputField}/>
         </div>
         <div className={`${styles.wrapInput} ${styles.enterRegistrationField}`}>
@@ -23,7 +23,7 @@ const AuthWindow = () => {
         </div>
         <NavigationLink className={styles.reg} hrefLink="/">Узнать о нас больше</NavigationLink>
       </form>
-    </section>
+    </div>
   );
 };
 
