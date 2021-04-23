b9c448c048a75a9c363860de255ccd6a093673a7
Sokol <silvana93@mail.ru>
2021-04-23 22:39:26 +0300

    create GET-request

--- src/components/AuthWindow/index.jsx
+++ src/components/AuthWindow/index.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
@@ -11,45 +11,47 @@ const AuthWindow = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  function handleEmail(e){
+  function handleEmail(e) {
     setLogin(e.target.value);
   }
 
-  function handlePassword(e){
+  function handlePassword(e) {
     setPassword(e.target.value);
   }
 
-  async function handleSubmit(e){
+  async function handleSubmit(e) {
     e.preventDefault();
     setIsLoading(true);
-    try{
-    const response = await fetch(
-      "https://web-interactive.herokuapp.com/login",
-      {method:'POST',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8'
-        },
-      body:JSON.stringify({
-        login:login,
-        password:password
-        }),
+    try {
+      const response = await fetch(
+        "https://web-interactive.herokuapp.com/login",
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+          },
+          credentials: 'include',
+          body: JSON.stringify({
+            login: login,
+            password: password
+          }),
+        }
+      );
+
+      if (response.ok) {
+        setErrorMessage("Успех");
       }
-    );
+      else {
+        let responseJson = await response.json();
+        setErrorMessage(responseJson.text);
+      }
 
-    if(response.ok){
-      setErrorMessage("Успех");
+    } catch (err) {
+      setErrorMessage("провал на уровне мировой сети, прошу винить во всем рептилойдов");
+    } finally {
+      setIsLoading(false);
     }
-    else{
-     let responseJson = await response.json();
-     setErrorMessage(responseJson.text);
-    }
-
-  }catch(err){
-    setErrorMessage("провал на уровне мировой сети, прошу винить во всем рептилойдов");
-  }finally{
-    setIsLoading(false);
   }
-    }
 
 
   return (
@@ -59,18 +61,18 @@ const AuthWindow = () => {
 
         <div className={styles.wrapInput}>
           <label htmlFor="emailField" className={styles.labelText}>Логин</label>
-          <input id="emailField" type="text" className={styles.inputField} onChange={handleEmail}/>
+          <input id="emailField" type="text" className={styles.inputField} onChange={handleEmail} />
         </div>
 
         <div className={styles.wrapInput}>
           <label htmlFor="passwordField" className={styles.labelText}>Пароль</label>
-          <input id="passwordField" type="password" className={styles.inputField} onChange={handlePassword}/>
+          <input id="passwordField" type="password" className={styles.inputField} onChange={handlePassword} />
         </div>
 
         <p className={styles.errorMessage}>{errorMessage}</p>
 
         <div className={`${styles.wrapInput} ${styles.enterRegistrationField}`}>
-          <button disabled={(login.length >= 2 && password.length >=5) ? false : true} className={styles.enterButton}>{ isLoading ? "Загрузка..." : "Войти" } </button>
+          <button disabled={(login.length >= 2 && password.length >= 5) ? false : true} className={styles.enterButton}>{isLoading ? "Загрузка..." : "Войти"} </button>
           <DefaultLink hrefLink="/registration">Регистрация</DefaultLink>
         </div>
 
--- src/components/RightMainBar/index.jsx
+++ src/components/RightMainBar/index.jsx
@@ -6,43 +6,81 @@ import styles from './styles.module.scss';
 import NavigationLink from 'components/NavigationLink';
 import avatar from '../../images/Avatar.jpg'
 
-const RightMainBar = () => {
-  return (
-    <ul className={styles.loginBarList}>
-      <li className={styles.avatarList}>
-      <img alt="avatar" src={avatar} className={styles.avatar}></img>
+class RightMainBar extends React.Component {
+
+  componentDidMount(){
+    fetch(
+      "https://web-interactive.herokuapp.com/user-info",
+      {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        credentials: 'include',
+      }
+    ).then((result) => {
+      if (result.ok) {
+        console.log("Все прошло успешно");
+      }
+      else {
+        console.log("результат не получен");
+        return result.json();
+      }
+    }, (error) => {
+      console.log("все пропало!" + error);
+    }
+    ).then((data) => {
+      if (data) {
+        console.log(data);
+      }
+    }
+    )
+
+
+    // fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
+    // .then(response => response.json())
+    // .then(commits => alert(commits[0].author.login));
+  }
+
+
+  render() {
+    return (
+      <ul className={styles.loginBarList}>
+        <li className={styles.avatarList}>
+          <img alt="avatar" src={avatar} className={styles.avatar}></img>
+        </li>
+
+        <li className={styles.name}>
+          Имя
       </li>
 
-      <li className={styles.name}>
-        Имя
+        <li className={styles.secondName}>
+          Фамилия
       </li>
 
-      <li className={styles.secondName}>
-        Фамилия
+        <li className={styles.rate}>
+          Тариф
       </li>
 
-      <li className={styles.rate}>
-        Тариф
+        <li className={styles.remainder}>
+          Остаток срока <br />подписки
       </li>
 
-      <li className={styles.remainder}>
-        Остаток срока <br/>подписки
-      </li>
-
-      <li className={styles.profileLink}>
-        <NavigationLink className={styles.rightMainLink} hrefLink="/profile">
-          Мой профиль
+        <li className={styles.profileLink}>
+          <NavigationLink className={styles.rightMainLink} hrefLink="/profile">
+            Мой профиль
         </NavigationLink>
-      </li>
+        </li>
 
-      <li>
-        <NavigationLink className={styles.rightMainLink} hrefLink="/">
-          Выйти
+        <li>
+          <NavigationLink className={styles.rightMainLink} hrefLink="/">
+            Выйти
         </NavigationLink>
-      </li>
-    </ul>
-  );
-};
+        </li>
+      </ul>
+    );
+  }
+}
 
 RightMainBar.propTypes = {
   children: PropTypes.node,
